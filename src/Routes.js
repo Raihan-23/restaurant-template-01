6d9e38ffb8081d7769b5ae5bddfee260edade00a
mdasrafuzzamanopu <mdasrafuzzamanopu@gmail.com>
2024-11-03 21:16:29 +0600

    update codes files

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import Home1 from "pages/Home1";
-import RecipeDetails from "pages/RecipeDetails";
-import BlogList from "pages/BlogList";
-import BlogPost from "pages/BlogPost";
-import Contact from "pages/Contact";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Home1 from "./pages/Home1";
+// import RecipeDetails from "./pages/RecipeDetails";
+// import BlogList from "./pages/BlogList";
+// import BlogPost from "./pages/BlogPost";
+// import Contact from "./pages/Contact";
 
 const ProjectRoutes = () => {
     let element = useRoutes([
         { path: "/", element: <Home /> },
         { path: "*", element: <NotFound /> },
         { path: "home1", element: <Home1 /> },
-        { path: "recipedetails", element: <RecipeDetails /> },
-        { path: "bloglist", element: <BlogList /> },
-        { path: "blogpost", element: <BlogPost /> },
-        { path: "contact", element: <Contact /> },
+        // { path: "recipedetails", element: <RecipeDetails /> },
+        // { path: "bloglist", element: <BlogList /> },
+        // { path: "blogpost", element: <BlogPost /> },
+        // { path: "contact", element: <Contact /> },
     ]);
 
     return element;
